77eec782963c2b1dbf62599f6c2dff3111c51368
eddydn <eddydn@gmail.com>
2016-11-21 16:41:00 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GIFSplashScreen
+{
+    [Activity(Label = "GIFSplashScreen",Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
